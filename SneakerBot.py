48eed7e557af757ffbe47ed44c3985ab681dded7
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-05-14 10:21:03 +0200

    Bot project completed

--- SneakerBot.py
+++ SneakerBot.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 import undetected_chromedriver.v2 as uc
