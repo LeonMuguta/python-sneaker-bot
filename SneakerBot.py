b4e1b293e908f106d4357e087cb0bb372aafee1e
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-05-14 10:19:01 +0200

    Bot program completed

--- SneakerBot.py
+++ SneakerBot.py
@@ -7,6 +7,49 @@ import time
 import threading
 from datetime import datetime
 
+def executeBot():
+        if __name__ == '__main__':
+                options = webdriver.ChromeOptions()              
+                options.add_experimental_option("excludeSwitches", ["enable-automation"])
+                options.add_experimental_option("useAutomationExtension", False)
+                options.add_argument("--disable-blink-features=AutomationControlled")
+                options.add_argument("--headless")
+                options.page_load_strategy = 'eager'
+
+                driver = uc.Chrome()
+                driver.maximize_window()
+
+                stealth(driver,
+                        languages=["en-US", "en"],
+                        vendor="Google Inc.",
+                        platform="Win32",
+                        webgl_vendor="Intel Inc.",
+                        renderer="Intel Iris OpenGL Engine",
+                        fix_hairline=True,
+                        )
+
+                with driver:
+                        driver.get('https://shop.adidas.co.za/campaign/unisex/yeezy/')
+
+                time.sleep(10)
+                driver.find_element(by=By.XPATH, value='/html/body/div[3]/div[2]/button[1]').click()
+                time.sleep(0.5)
+                driver.find_element(by=By.XPATH, value='//*[@id="catalog-index-page"]/div[2]/div/div/div/div[1]/div/a/div/img').click()
+                time.sleep(0.5)
+                # Click on size
+                driver.find_element(by=By.XPATH, value='//*[@id="OptionsSingleDefault"]/div[2]/div[1]/ul/li/div/div/ul/li[9]').click()
+                time.sleep(0.5)
+                # Add to Bag
+                driver.find_element(by=By.XPATH, value='//*[@id="addtocart-OR453SH93TEKZAFAS"]').click()
+                time.sleep(2)
+                # "Checkout"
+                driver.find_element(by=By.XPATH, value='//*[@id="cartReservationModal"]/div/div/div[3]/div/a').click()
+                time.sleep(0.5)
+                driver.find_element(by=By.XPATH, value='//*[@id="cart-items-list-form"]/div/div[2]/div[2]/div[2]/div/a').click()
+                # 6 minutes given to complete transaction before window closes
+                time.sleep(360)
+                driver.quit()
+
 def checkTime():
         threading.Timer(1, checkTime).start()
 
@@ -15,85 +58,7 @@ def checkTime():
         current_time = now.strftime("%H:%M:%S")
         print("Current Time =", current_time)
 
-        if(current_time == '16:31:00'):
-                if __name__ == '__main__':
-                        options = webdriver.ChromeOptions()              
-                        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-                        options.add_experimental_option("useAutomationExtension", False)
-                        options.add_argument("--disable-blink-features=AutomationControlled")
-                        options.add_argument("--headless")
-                        options.page_load_strategy = 'eager'
-
-                        driver = uc.Chrome()
-                        driver.maximize_window()
-
-                        stealth(driver,
-                                languages=["en-US", "en"],
-                                vendor="Google Inc.",
-                                platform="Win32",
-                                webgl_vendor="Intel Inc.",
-                                renderer="Intel Iris OpenGL Engine",
-                                fix_hairline=True,
-                                )
-
-                        with driver:
-                                driver.get('https://nowsecure.nl')
-
-                        print("WE IN!")
-                        time.sleep(30)
-                        driver.quit()
+        if(current_time == '10:06:30'):
+               executeBot() 
 
 checkTime()
-
-# if __name__ == '__main__':
-#         options = webdriver.ChromeOptions()
-#         options.add_argument("start-maximized")
-#         # options.add_argument("window-size=1280,800")
-#         options.add_experimental_option("excludeSwitches", ["enable-automation"])
-#         options.add_experimental_option("useAutomationExtension", False)
-#         options.add_argument("--disable-blink-features=AutomationControlled")
-#         # options.add_argument("--headless")
-#         # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
-#         options.page_load_strategy = 'eager'
-
-#         driver = uc.Chrome()
-        # driver = webdriver.Chrome(chrome_options=options)
-        # , executable_path=r'C:\\Users\\Leon Muguta\\Desktop\\sneaker-bot\\chromedriver.exe') 
-
-        # stealth(driver,
-        #         languages=["en-US", "en"],
-        #         vendor="Google Inc.",
-        #         platform="Win32",
-        #         webgl_vendor="Intel Inc.",
-        #         renderer="Intel Iris OpenGL Engine",
-        #         fix_hairline=True,
-        #         )
-
-
-        # driver.get("https://shop.adidas.co.za/")
-        # driver.get("https://nowsecure.nl")
-        # driver.get('https://shop.adidas.co.za/blog/yeezy/')
-        # with driver:
-        #         driver.get('https://nowsecure.nl')
-
-        # print("WE IN!")
-        # time.sleep(30)
-        # driver.quit()
-
-
-        # driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[2]/button[1]").click()
-        # time.sleep(0.5)
-        # driver.find_element(by=By.XPATH, value='//*[@id="all-men-best"]/div/div[2]/div/div[2]/a/div/button').click()
-        # time.sleep(100)
-
-        # time.sleep(0.5)
-        # driver.find_element_by_xpath("/html/body/div[3]/div[2]/button[1]").click()
-        # time.sleep(0.5)
-        # driver.find_element_by_xpath('//*[@id="all-men-best"]/div/div[2]/div/div[2]/a/div/button').click()
-        # time.sleep(7)
-        # driver.find_element_by_xpath('//*[@id="loop-container"]/div/article/div[2]/div[2]/div[4]/div/a').click()
-        # handle = driver.current_window_handle
-        # driver.service.stop()
-        # time.sleep(6)
-        # driver = webdriver.Chrome(options=options)
-        # driver.switch_to.window(handle)
